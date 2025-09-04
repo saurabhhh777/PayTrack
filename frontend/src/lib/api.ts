5610daa0b856d57eb3fd07ef1a1ad85defb7f6c9
saurabhhh777 <jeeaspirantwallah2022@gmail.com>
2025-09-04 21:27:26 +0530

    typo fix:

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: (import.meta as any).env.VITE_BACKEND_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
